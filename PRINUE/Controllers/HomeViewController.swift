74a47483eda74f50ba546880646a75fb12d6a32a
iiRazan <Razan.Aljohani.med.s@tuwaiq.edu.sa>
2022-01-01 15:40:15 +0300

    - category collection view setup (#6)

--- PRINUE/Controllers/HomeViewController.swift
+++ PRINUE/Controllers/HomeViewController.swift
@@ -6,24 +6,59 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let storage = Storage.storage()
+//    let storageRef = storage.reference()
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let categoryCell = collectionView.dequeueReusableCell(withReuseIdentifier: "CategoryID", for: indexPath) as! CategoryCell
+        categoryCell.categoryImage.layer.cornerRadius = 30
+        categoryCell.categoryImage.layer.masksToBounds = true
+//        categoryCell.comingSoonLabel.layer.cornerRadius = 30
+//        categoryCell.categoryImage.image =
+//        categoryCell.categoryTitle = ""
+        return categoryCell
     }
-    */
+    
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
 
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        categoryCollectionView.delegate = self
+        categoryCollectionView.dataSource = self
+    }
+    
+//
+//    func downloadAll() {
+//        let folderUrl = "gs://styleyapp.appspot.com/Clothes"
+//        let storageRef = Storage.storage().reference(forURL: folderUrl)
+//
+//        storageRef.listAll { result, error in
+//          print ("Items in your Folder: ", result.items.count)
+//          for item in result.items {
+//            print ("name: ---: ", item.fullPath)
+//
+//            item.getData(maxSize: Int64.max) { data, err in
+//
+//
+//              guard let data = data else { return }
+//
+//              let cloth = clothes(imgClothes: UIImage(data: data)!, textClothes: item.name)
+//              self.arrClothes.append(cloth)
+//
+//              DispatchQueue.main.async {
+//                self.clothesCV.reloadData()
+//              }
+//            }
+//          }
+//        }
 }
