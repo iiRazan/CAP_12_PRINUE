d6d6bc9d96df43622c8743dfe45c14faac41716e
iiRazan <irazan.m24@gmail.com>
2022-01-01 15:38:34 +0300

    - category collection view setup

---
+++ PRINUE/Views/CategoryCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCell.swift
+//  PRINUE
+//
+//  Created by Razan on 26/05/1443 AH.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+    
+    @IBOutlet weak var categoryImage : UIImageView!
+    @IBOutlet weak var categoryTitle : UILabel!
+    @IBOutlet weak var comingSoonLabel : UILabel!
+    
+    
+}
